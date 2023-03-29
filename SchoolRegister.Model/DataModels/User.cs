a080132f57e124f156451ce9deb09b4313a056e2
OrhunBegen <orhunbegen@outlook.com>
2023-03-29 14:42:50 +0200

    data models scripetd

--- SchoolRegister.Model/DataModels/User.cs
+++ SchoolRegister.Model/DataModels/User.cs
@@ -1,11 +1,12 @@
-using Microsoft.AspNetCore.Identity;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
 namespace SchoolRegister.Model.DataModels
 {
-public class User : IdentityUser<int>
-{
-public string FirstName { get; set; } = default!;
-public string LastName { get; set; } = default!;
-public DateTime RegistrationDate { get; set; }
-}
+    public class User
+    {
+        
+    }
 }
