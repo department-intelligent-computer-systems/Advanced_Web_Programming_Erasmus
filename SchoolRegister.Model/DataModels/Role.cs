f39c3e5df969b976c9ef1023af1d83940af72770
BerkeCan <berkecanpeker4@gmail.com>
2023-03-29 15:13:15 +0200

    Update of Role.cs

--- SchoolRegister.Model/DataModels/Role.cs
+++ SchoolRegister.Model/DataModels/Role.cs
@@ -6,5 +6,10 @@ namespace SchoolRegister.Model.DataModels
     public class Role : IdentityRole<int>
     {
         public RoleValue RoleValue {get; set;} = default;
+
+        public Role(String name , RoleValue roleValue){
+            this.RoleValue = roleValue;
+            this.Name = name;
+        }
     }
 }
