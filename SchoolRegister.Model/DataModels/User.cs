f4f56e884c787c03a9c0caaf17963f4eeba0401b
OrhunBegen <orhunbegen@outlook.com>
2023-04-05 14:39:54 +0200

    user.cs update

--- SchoolRegister.Model/DataModels/User.cs
+++ SchoolRegister.Model/DataModels/User.cs
@@ -1,12 +1,11 @@
+using Microsoft.AspNetCore.Identity;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace SchoolRegister.Model.DataModels
 {
-    public class User
-    {
-        
-    }
+public class User : IdentityUser<int>
+{
+public string FirstName { get; set; } = default!;
+public string LastName { get; set; } = default!;
+public DateTime RegistrationDate { get; set; }
+}
 }
